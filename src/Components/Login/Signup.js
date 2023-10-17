6ae4fec8b7f011ac1ecaf1f737511beb4653595c
unknown <sharif009009@gmail.com>
2023-10-17 15:32:16 +0600

    feat: login users

--- src/Components/Login/Signup.js
+++ src/Components/Login/Signup.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../../images/signup-removebg-preview.png";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const navigate = useNavigate;
+
+  const [name, setName] = useState();
+  const [userName, setUserName] = useState();
+  const [phoneNo, setPhoneNO] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [role, setRole] = useState("employee");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!name || !userName) {
+      toast.error("pleaase fill all field");
+    }
+    try {
+      const config = {
+        headers: {
+          "content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "http://localhost:5000/allusers",
+        {
+          name,
+          userName,
+          phoneNo,
+          email,
+          password,
+          confirmPassword,
+          role,
+        },
+        config
+      );
+      toast.success("Account created");
+
+      setName("");
+      setUserName("");
+      setPhoneNO("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      setRole("");
+
+      // console.log(data);
+    } catch (error) {
+      // console.log(error);
+      toast.error("Something Went Worng");
+    }
+  };
   return (
     <div>
+      <Toaster position="top-center" reverseOrder={false} />
       <div className=" container mx-auto grid lg:grid-cols-2 gap-2 items-center">
         <div className="loginImage">
           <img src={img} alt="" />
@@ -18,34 +72,46 @@ const Signup = () => {
             <input
               type="text"
               placeholder="Your Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="input input-bordered input-md w-full max-w-xs"
             />
 
             <input
               type="text"
               placeholder="Username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
               className="input input-bordered input-md w-full max-w-xs"
             />
 
             <input
-              type="text"
+              type="number"
               placeholder="Phone No"
+              value={phoneNo}
+              onChange={(e) => setPhoneNO(e.target.value)}
               className="input input-bordered input-md w-full max-w-xs"
             />
 
             <input
-              type="text"
+              type="email"
               placeholder="Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="input input-bordered input-md w-full max-w-xs"
             />
             <input
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="input input-bordered input-md w-full max-w-xs"
             />
             <input
               type="password"
               placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               className="input input-bordered input-md w-full max-w-xs"
             />
             {/* ------------------------------------------------ */}
@@ -57,7 +123,8 @@ const Signup = () => {
                     required
                     type="radio"
                     name="radio-10"
-                    value="----"
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
                     className=" radio checked:bg-blue-500"
                   />
                   <span className="label-text ml-3">Employee</span>
@@ -73,6 +140,7 @@ const Signup = () => {
             <button
               className="custom-button w-10/12"
               type="submit"
+              onClick={submitHandler}
               // isLoading={loading}
             >
               Signup
