fb61865a4aa43e7d2167c214d936bfc33688cd9f
Sharif101 <sharif009009@gmail.com>
2023-10-21 11:20:31 +0600

    fect: delete and preview

--- src/Components/Users/Singleuser.js
+++ src/Components/Users/Singleuser.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaEye, FaTrash, FaPen } from "react-icons/fa";
+import UserPreviewModal from "../AllModals/UserPreviewModal";
 
-const Singleuser = ({ d }) => {
+const Singleuser = ({ d, handleDelete }) => {
+  let { _id } = d;
+  const [modal, setModal] = useState(false);
+
   return (
-    <div className="p-2 mt-3 flex justify-between items-center items-center foodicons dashboardText">
-      <h1 className="w-96 font-medium">{d.name}</h1>
-      <h1 className="w-96  text-center">{d.email}</h1>
-      <h1 className="w-96  text-center">{d.phoneNo}</h1>
-      <h1 className="w-96 flex items-center justify-around text-center">
-        <div>
-          <button>
-            <FaEye />
-          </button>
-        </div>
-        <div>
-          <button>
-            <FaTrash />
-          </button>
-        </div>
-      </h1>
+    <div>
+      <div className="p-2 mt-3 flex justify-between items-center items-center foodicons dashboardText">
+        <h1 className="w-96 font-medium">{d.name}</h1>
+        <h1 className="w-96  text-center">{d.email}</h1>
+        <h1 className="w-96  text-center">{d.phoneNo}</h1>
+        <h1 className="w-96 flex items-center justify-around text-center">
+          <div>
+            <button
+              onClick={() => {
+                setModal(true);
+              }}
+            >
+              <FaEye />
+            </button>
+          </div>
+          <div>
+            <button onClick={() => handleDelete(_id)}>
+              <FaTrash />
+            </button>
+          </div>
+        </h1>
+      </div>
+
+      {/* ---------------------------- */}
+      {modal && <UserPreviewModal setModal={setModal} _id={_id} />}
     </div>
   );
 };
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Singleuser from "./Singleuser";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const Users = () => {
   const [data, setData] = useState([]);
@@ -10,8 +12,32 @@ const Users = () => {
       .then((data) => setData(data));
   }, [data]);
 
+  // delete single users
+  const handleDelete = async (_id) => {
+    const proceed = window.confirm("Are you sure to delete this");
+    try {
+      if (proceed) {
+        const config = {
+          headers: {
+            "content-type": "application/json",
+          },
+        };
+        const { data } = await axios.delete(
+          `http://localhost:5000/allusers/${_id}`,
+          config
+        );
+        toast.success("Successfully Deleted!");
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error.response.data.msg);
+      toast.error("Something Went Worng");
+    }
+  };
+
   return (
     <div className="food-list">
+      <Toaster position="top-center" reverseOrder={false} />
       {/* ----------------------------------- */}
       <div className="menu-list">
         <div className="menu-list-titles mt-8 mb-8 flex justify-between items-center">
@@ -23,7 +49,7 @@ const Users = () => {
       </div>
       {/* ------------- */}
       {data.map((d) => (
-        <Singleuser d={d} key={d._id} />
+        <Singleuser d={d} key={d._id} handleDelete={handleDelete} />
       ))}
     </div>
   );
