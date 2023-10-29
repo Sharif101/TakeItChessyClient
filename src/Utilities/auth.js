80986735b47e87e182a562b36ad07aaa731b9584
unknown <sharif009009@gmail.com>
2023-10-29 19:49:40 +0600

    PC: make cookies for auth logout and user

---
+++ src/Utilities/auth.js
@@ -0,0 +1,32 @@
+import cookie from "js-cookie";
+import axios from "axios";
+
+export const setCookie = (key, value) => {
+  cookie.set(key, value, { expires: 2 });
+};
+
+export const removeCookie = (key) => {
+  cookie.remove(key);
+};
+
+export const getCookie = (key) => {
+  return cookie.get(key);
+};
+
+export const setAuthentication = (token) => {
+  setCookie("token", token);
+};
+
+export const logOut = () => {
+  removeCookie("token");
+};
+
+export const isLogin = async () => {
+  const token = getCookie("token");
+
+  if (token) {
+    const res = await axios.post("http://localhost:5000/auth", { token });
+    return res.data;
+  }
+  return false;
+};
