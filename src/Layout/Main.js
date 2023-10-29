97f6a3c830dbe667ba716c73390288f9c518660c
unknown <sharif009009@gmail.com>
2023-10-29 19:55:22 +0600

    PC: add toastfy

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Shared/Header";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const Main = () => {
   return (
     <div>
       <Header></Header>
       <div>
+        <Toaster position="top-center" reverseOrder={false} />
         <Outlet></Outlet>
       </div>
     </div>
